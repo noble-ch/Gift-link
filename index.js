351873245d4bf130492f78559f0458b664cc7dd5
Noble-ch <123499650+Noble-ch@users.noreply.github.com>
2023-03-10 15:49:06 +0300

    updated

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const inputField = document.getElementById("inputField");
+const clickDiv = document.getElementById("img");
+const displayText = document.getElementById("displayText");
+var message = document.getElementById("message");
 function theGift() {
   var box = document.querySelector("#img");
   box.style.backgroundImage = "url('middle.gif')";
-  var message = document.getElementById("message");
   message.textContent = " With ALL Do respect";
   var htwo = document.querySelector("h2");
   htwo.style.fontSize = "32px";
   htwo.style.margin = "0";
   htwo.style.borderRadius = " 20px";
 }
+
+clickDiv.addEventListener("click", () => {
+  if (inputField.value !== "") {
+    displayText.textContent = "Fuck you " + inputField.value;
+    theGift();
+    message.style.background = "rgb(253, 219, 163)";
+  } else {
+    message.textContent = " please enter your name";
+    message.style.background = "red";
+  }
+});
