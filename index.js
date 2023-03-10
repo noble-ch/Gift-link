c2721a96291bb9b9b860675f22c2a4e2dcba4138
Noble-ch <123499650+Noble-ch@users.noreply.github.com>
2023-03-10 17:46:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -15,38 +15,10 @@ function theGift() {
 clickDiv.addEventListener("click", () => {
   if (inputField.value !== "") {
     if (
-      inputField.value !== "NOBLE" &&
-      inputField.value !== "NOB" &&
-      inputField.value !== "NOBA" &&
-      inputField.value !== "NOBEL" &&
-      inputField.value !== "NOBL" &&
-      inputField.value !== "NO" &&
-      inputField.value !== "NOBLE BIRU" &&
-      inputField.value !== "NOBLEBIRU" &&
-      inputField.value !== "N" &&
-      inputField.value !== "NOBELA" &&
-      inputField.value !== "NOBLE BIRU DEGEFA" &&
-      inputField.value !== "NOBLEB" &&
-      inputField.value !== "NOBLE B" &&
-      inputField.value !== "NOBLE BI" &&
-      inputField.value !== "NOBLE BIR" &&
-      inputField.value !== "NOBLE BIRRU" &&
-      inputField.value !== "noble" &&
-      inputField.value !== "nob" &&
-      inputField.value !== "noba" &&
-      inputField.value !== "nobel" &&
-      inputField.value !== "nobl" &&
-      inputField.value !== "no" &&
-      inputField.value !== "noble biru" &&
-      inputField.value !== "noblebiru" &&
-      inputField.value !== "n" &&
-      inputField.value !== "nobela" &&
-      inputField.value !== "noble biru degefa" &&
-      inputField.value !== "noble" &&
-      inputField.value !== "noble b" &&
-      inputField.value !== "noble bi" &&
-      inputField.value !== "noble birr" &&
-      inputField.value !== "noble birru"
+      inputField.value == "meheretu" ||
+      inputField.value == "bisrat" ||
+      inputField.value == "dolphin" ||
+      inputField.value == "firaol"
     ) {
       displayText.textContent = "Fuck you " + inputField.value;
       theGift();
@@ -54,13 +26,13 @@ clickDiv.addEventListener("click", () => {
       message.style.fontFamily = "got";
       message.style.color = "rgb(130, 95, 48)";
     } else {
-      message.textContent = " wrong input 🖕";
+      message.textContent = " wrong password";
       message.style.color = "white";
       message.style.fontFamily = "inherit";
       message.style.background = "red";
     }
   } else {
-    message.textContent = " please enter your name";
+    message.textContent = " please enter your password";
     message.style.color = "white";
     message.style.fontFamily = "inherit";
     message.style.background = "red";
