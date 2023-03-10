b8da4f5672aac8062aa8379f706f230e072a576c
Noble-ch <123499650+Noble-ch@users.noreply.github.com>
2023-03-10 14:29:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function theGift() {
+  var box = document.querySelector("#img");
+  box.style.backgroundImage = "url('middle.gif')";
+  var message = document.getElementById("message");
+  message.textContent = " With ALL Do respect";
+  var htwo = document.querySelector("h2");
+  htwo.style.fontSize = "32px";
+  htwo.style.margin = "0";
+  htwo.style.borderRadius = " 20px";
+}
